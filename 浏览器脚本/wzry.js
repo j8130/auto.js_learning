573ff1f9eac82df5c69d3f0c01d803979338cd70
j8130 <122969720@qq.com>
2020-09-21 04:12:40 +0800

    浏览器打通

---
+++ 浏览器脚本/wzry.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Test1
+// @namespace    https://github.com/j8130
+// @version      0.1
+// @description  try to take over the world!
+// @author       jsy
+// @match        https://github.com/*
+// @grant        none
+// @require      http://code.jquery.com/jquery-1.11.1.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    alert("hello world")
+
+    // Your code here...
+})();
+
